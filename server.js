a856d7cf0568a87a4d06bc88cd5265b42816753a
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-09-20 10:18:43 -0400

    work mode app

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-const express = require("express");
-const app = express;
-const PORT = 3030;
-const cors = require("cors");
-const mongoose = require("mongoose");
-const toDoList = require("./models/todolist");
-require("dotenv").config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+require('dotenv').config({path: './config/.env'})
 
-mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true },
-  (err) => {
-    if (err) {
-      return console.error(err);
-    }
-    console.log("Connected to database!");
-  }
-);
+// Passport config
+require('./config/passport')(passport)
 
-app.get("/", async (req, res) => {
-  try {
-    toDoList.find({}, (err, tasks) => {
-      res.render("index.ejs", {
-        TaskList: tasks,
-      });
-    });
-  } catch (err) {
-    if (err) return res.status(500).send(err);
-  }
-  //db.collection("tasks")
-  //  .find.toArray()
-  //  .then((data) => {
-  //    res.render("index.ejs", { info: data });
-  //  })
-  //  .catch((error) => console.error(error));
-});
+connectDB()
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
